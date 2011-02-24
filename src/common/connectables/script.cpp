09c39a5d8aad1d8500a31b2f298c783ca64493f2
CtrlBrk <ctrlbrk76@gmail.com>
2011-02-24 15:15:11 +0100

    problem with assume-unchanged ?
    Signed-off-by: CtrlBrk <ctrlbrk76@gmail.com>

--- src/common/connectables/script.cpp
+++ src/common/connectables/script.cpp
@@ -6,6 +6,6 @@ using namespace Connectables;
 Script::Script(MainHost *host, int index, const ObjectInfo &info) :
     Object(host,index,info)
 {
-//    QString prog="Programs.ChangeProg(2);";
-//    debug2(<< "script:" << myHost->scriptEngine.evaluate(prog).toString())
+    QString prog="Programs.ChangeProg(2);";
+    debug2(<< "script:" << myHost->scriptEngine.evaluate(prog).toString())
 }
